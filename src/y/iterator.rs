1f09d9f9638eaab2d1f4c8584983a44cdf9338ca
Rg <daimaldd@gmail.com>
2022-09-10 18:56:03 +0800

    :coffee:

--- src/y/iterator.rs
+++ src/y/iterator.rs
@@ -5,46 +5,31 @@ use crate::skl::Chunk;
 use std::io::Cursor;
 use std::marker::PhantomData;
 use std::mem::size_of;
+use std::ptr::NonNull;
 use std::slice::{from_raw_parts, from_raw_parts_mut, Iter};
 use crate::skl::BlockBytes;
 
 /// ValueStruct represents the value info that can be associated with a key, but also the internal
 /// Meta field.
 /// |meta|user_meta|cas_counter|value_size|value|
-#[derive(Debug)]
+#[derive(Debug, Default, PartialEq)]
 #[repr(C)]
 pub struct ValueStruct {
     pub(crate) meta: u8,
     pub(crate) user_meta: u8,
     pub(crate) cas_counter: u64,
-    pub(crate) value: BlockBytes,
+    pub(crate) value:  Vec<u8>,
 }
-
-impl Default for ValueStruct {
-    fn default() -> Self {
-        // ValueStruct {
-        //     meta: 0,
-        //     user_meta: 0,
-        //     cas_counter: 0,
-        //     value: Slice::new(0 as *const u8, 0),
-        // }
-        todo!()
-    }
-}
-
-// impl From<T> for ValueStruct where T: Chunk {
-//     fn from(chunk: T) -> Self {
-//         let data = chunk.get_data();
-//     }
-// }
 
 impl ValueStruct {
     const fn header_size() -> usize {
         10
     }
+
     fn size(&self) -> usize {
-        Self::header_size() + self.value.size()
+        Self::header_size() + self.value.len()
     }
+
     pub(crate) fn to_vec(&self) -> Vec<u8> {
         let mut buffer = vec![0u8; self.size()];
         buffer[0] = self.meta;
@@ -53,46 +38,27 @@ impl ValueStruct {
             let mut cursor = Cursor::new(&mut buffer[2..ValueStruct::header_size()]);
             cursor.write_u64::<BigEndian>(self.cas_counter);
         }
-        buffer[ValueStruct::header_size()..].copy_from_slice(self.value.get_data());
+        buffer[ValueStruct::header_size()..].copy_from_slice(&self.value);
         buffer
     }
 }
-//
-// impl From<Slice> for ValueStruct {
-//     fn from(slice: Slice) -> Self {
-//         let data = slice.get_data();
-//         let total_sz = data.len();
-//         let mut value = ValueStruct::default();
-//         value.meta = data[0];
-//         value.user_meta = data[1];
-//         value.cas_counter = Cursor::new(&data[2..ValueStruct::header_size()])
-//             .read_u64::<BigEndian>()
-//             .unwrap();
-//         {
-//             let ptr = data[ValueStruct::header_size()..].as_ptr();
-//             value.value = Slice::new(ptr, total_sz - ValueStruct::header_size());
-//         }
-//         value
-//     }
-// }
 
 impl From<&[u8]> for ValueStruct {
-    fn from(_: &[u8]) -> Self {
-        todo!()
+    fn from(v: &[u8]) -> Self {
+        let cas_counter  = Cursor::new(&v[2..10]).read_u64::<BigEndian>().unwrap();
+        let value_sz = Cursor::new(&v[10..14]).read_u32::<BigEndian>().unwrap();
+        ValueStruct{
+           meta: v[0],
+           user_meta: v[1],
+           cas_counter,
+           value: v[ValueStruct::header_size()..].to_vec(),
+       }
     }
 }
 
 impl Into<Vec<u8>> for ValueStruct {
     fn into(self) -> Vec<u8> {
-        let mut buffer = vec![0u8; self.size()];
-        buffer[0] = self.meta;
-        buffer[1] = self.user_meta;
-        {
-            let mut cursor = Cursor::new(&mut buffer[2..ValueStruct::header_size()]);
-            cursor.write_u64::<BigEndian>(self.cas_counter);
-        }
-        buffer[ValueStruct::header_size()..].copy_from_slice(self.value.get_data());
-        buffer
+       self.to_vec()
     }
 }
 
