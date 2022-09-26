5014e4ec8c58e9bfd5c597760682084c1f9524ec
Rg <daimaldd@gmail.com>
2022-09-27 01:16:09 +0800

    Revert "🌵 👎 🔢 " (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,3 @@
-#![feature(pointer_byte_offsets)]
-#![feature(sync_unsafe_cell)]
-#![feature(associated_type_defaults)]
-#![feature(type_alias_impl_trait)]
-#![feature(strict_provenance_atomic_ptr)]
-
-use std::mem::align_of;
-
-extern crate core;
-
 mod options;
 mod skl;
 mod y;
-
-#[allow(dead_code)]
-#[inline]
-pub(crate) fn must_align<T>(ptr: *const T) {
-    let actual = (ptr as usize) % align_of::<T>() == 0;
-    assert!(actual);
-}
-
-use thiserror::Error;
-
-#[derive(Debug, Error, PartialEq)]
-pub enum BadgerErr {}
