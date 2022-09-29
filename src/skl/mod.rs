e696378cce2cf0b421d859f9972636f648d2fc43
Rg <daimaldd@gmail.com>
2022-09-30 00:59:49 +0800

    fix compiled

--- src/skl/mod.rs
+++ src/skl/mod.rs
@@ -1,17 +1,16 @@
 mod alloc;
 mod arena;
 mod cursor;
-
 mod node;
 mod skip;
 pub mod small_allocate;
+
 use crate::skl::node::Node;
 use crate::y::ValueStruct;
 use crate::{must_align, BadgerErr};
 pub use alloc::{Allocate, BlockBytes, Chunk, SmartAllocate};
 pub use arena::Arena;
 pub use cursor::Cursor;
-use std::mem::size_of;
 
 const MAX_HEIGHT: usize = 20;
 const HEIGHT_INCREASE: u32 = u32::MAX / 3;
