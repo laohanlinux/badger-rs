5237b9f1f195d2eba0e850718a64b47565a073f2
Rg <daimaldd@gmail.com>
2023-10-12 19:07:21 +0800

    :dog:

--- src/skl/alloc.rs
+++ src/skl/alloc.rs
@@ -247,6 +247,11 @@ impl<T> Drop for OnlyLayoutAllocate<T> {
     }
 }
 
+pub struct VecAllocate {
+    cursor: Arc<AtomicUsize>,
+    cap: Arc<AtomicUsize>,
+}
+
 // The alloc is only supported on layout, and it only append
 #[derive(Debug)]
 pub struct SliceAllocate {
@@ -402,4 +407,138 @@ mod tests {
             assert_eq!(buffer[datum], datum as u8);
         }
     }
+}
+
+
+#[cfg(test)]
+mod testss{
+    use std::{
+        cell::Cell,
+        mem, ptr,
+        sync::atomic::{AtomicUsize, Ordering},
+    };
+
+    // Node 结构体
+    #[derive(Debug)]
+    #[repr(C)]
+    pub struct Node {
+        pub(crate) key_offset: u32,
+        pub(crate) key_size: u16,
+        pub(crate) height: u16,
+        pub(crate) value: u64,
+        pub(crate) tower: [u32; 4], // 假设最大高度为 4
+    }
+
+    // ArenaObject 枚举
+    #[repr(align(8))]
+    pub enum ArenaObject {
+        Node {
+            discriminant: u8,
+            data: Node,
+        },
+        Data {
+            discriminant: u8,
+            data: Vec<u8>,
+        },
+    }
+
+    const MAX_HEIGHT: usize = 4;
+
+    pub struct Arena {
+        len: AtomicUsize,
+        cap: Cell<usize>,
+        ptr: Cell<*mut u8>,
+    }
+
+    impl Arena {
+        pub fn new(cap: usize) -> Arena {
+            let mut buf: Vec<u64> = Vec::with_capacity(cap / 8);
+            let ptr = buf.as_mut_ptr() as *mut u8;
+            let cap = buf.capacity() * 8;
+            mem::forget(buf);
+            Arena {
+                len: AtomicUsize::new(8),
+                cap: Cell::new(cap),
+                ptr: Cell::new(ptr),
+            }
+        }
+
+        pub fn alloc_object(&self, object: ArenaObject) -> Option<usize> {
+            let size = std::mem::size_of_val(&object);
+            let offset = self.len.fetch_add(size, Ordering::SeqCst);
+            // let size = std::mem::size_of::<ArenaObject>();
+            println!("size: {}, {}", size, mem::size_of::<Node>());
+            // let offset = self.len.fetch_add(size, Ordering::SeqCst);
+
+            // 先检查是否需要扩展内存块，然后将 ArenaObject 复制到内存中
+            if offset + size > self.cap.get() {
+                self.grow();
+            }
+
+            unsafe {
+                let dest = self.ptr.get().add(offset);
+                std::ptr::copy_nonoverlapping(&object as *const ArenaObject as *const u8, dest, size);
+            }
+
+            Some(offset)
+        }
+
+        pub fn get_object(&self, offset: usize) -> Option<&ArenaObject> {
+            if offset == 0 {
+                None
+            } else {
+                let ptr = unsafe {self.ptr.get().add(offset) as *const ArenaObject};
+                Some(unsafe { &*ptr })
+            }
+        }
+
+        fn grow(&self) {
+            // 实现内存块的扩展逻辑
+            // 这里省略，可以根据需要实现内存扩展的逻辑
+        }
+    }
+
+    #[test]
+    fn main() {
+        // 创建一个 Node 对象
+        let node = Node {
+            key_offset: 0,
+            key_size: 10,
+            height: 3,
+            value: 42,
+            tower: [0; MAX_HEIGHT],
+        };
+
+        // 创建一个 Vec<u8> 数据
+        let data = vec![1, 2, 3, 4, 5];
+
+        // 创建 Arena 实例
+        let arena = Arena::new(1<< 12);
+
+        // 分配 Node 对象并获取偏移量
+        // 分配 Node 对象并获取偏移量
+        let node_object = ArenaObject::Node {
+            discriminant: 0, // 使用标签 0 表示 Node
+            data: node,
+        };
+        let node_offset = arena.alloc_object(node_object).unwrap();
+
+        // 分配 Vec<u8> 数据并获取偏移量
+        let data_object = ArenaObject::Data {
+            discriminant: 1, // 使用标签 1 表示 Data
+            data,
+        };
+        let data_offset = arena.alloc_object(data_object).unwrap();
+
+        // 获取存储在 Arena 中的 Node 对象
+        if let Some(object) = arena.get_object(node_offset) {
+            match object {
+                ArenaObject::Node { data, .. } => {
+                    println!("Stored Node: {:?}", data);
+                }
+                _ => println!("Invalid object type."),
+            }
+        }
+    }
+
 }
